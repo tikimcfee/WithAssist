bcd99aaa6bc870c254d3e6e24e58b3c121143b24
Ivan Lugo <ivan.lugo@knights.ucf.edu>
2023-03-18 17:19:31 -0400

    Initial Commit

---
+++ WithAssist/WithAssistApp.swift
@@ -0,0 +1,17 @@
+//
+//  WithAssistApp.swift
+//  WithAssist
+//
+//  Created by Ivan Lugo on 3/18/23.
+//
+
+import SwiftUI
+
+@main
+struct WithAssistApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ WithAssistUITests/WithAssistUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  WithAssistUITestsLaunchTests.swift
+//  WithAssistUITests
+//
+//  Created by Ivan Lugo on 3/18/23.
+//
+
+import XCTest
+
+final class WithAssistUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
